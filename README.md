cf9c1c1d265d997df15e6196231705e35127a783
Thousand <569937624@qq.com>
2020-06-09 23:24:25 +0800

    对demo01进行一个例子接口的解释说明，以及修改本github的学习说明

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-通过https://www.pythonf.cn/的教程学习fastapi
-通过里面的教程的例子，进程练习
 学习fastapi框架
-
 首先安装fastapi框架
-
 Uvicorn网络微服务框架
-
 官方文档
 https://fastapi.tiangolo.com
 Github
 https://github.com/tiangolo/fastapi
 
 pip install fastapi  -i http://pypi.douban.com/simple/
+
+本github上的所有demo均来自官方文档的例子，对官方文档例子进行测试和一些自己理解的说明
+
+也通过https://www.pythonf.cn/ 的教程结合来理解fastapi框架的知识
+
--- fastapi_demo01_helloWorld/main.py
+++ fastapi_demo01_helloWorld/main.py
@@ -8,6 +8,19 @@ def read_root():
     return {"Hello": "World"}
 
 
+# 使用GET请求: http://127.0.0.1:8000/
+# 返回结果：
+"""
+{"Hello":"World"}
+"""
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
