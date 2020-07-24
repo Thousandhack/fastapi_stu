45983bc5b3883b086ebc18267aa86a8a8f4ecb92
Thousandhack <1391053697@qq.com>
2020-07-24 21:36:42 +0800

    具体还不知道TestClient的作用

---
+++ fastapi_demo68_TestingEvents/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+app = FastAPI()
+
+items = {}
+
+
+@app.on_event("startup")
+async def startup_event():
+    items["foo"] = {"name": "Fighters"}
+    items["bar"] = {"name": "Tenders"}
+
+
+@app.get("/items/{item_id}")
+async def read_items(item_id: str):
+    return items[item_id]
+
+
+def test_read_items():
+    with TestClient(app) as client:
+        response = client.get("/items/foo")
+        assert response.status_code == 200
+        assert response.json() == {"name": "Fighters-----"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    """
+    具体不知道这个测试的实现意义
+    """
+    uvicorn.run(app, host="0.0.0.0", port=8000)
